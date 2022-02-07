24f32ca3ed6a87010ce94a9727aa92499750170d
Sevi <correosevi@gmail.com>
2022-02-07 11:33:20 +0000

    Add animation to game over screen

--- src/components/GameOverScreen/index.js
+++ src/components/GameOverScreen/index.js
@@ -1,7 +1,9 @@
 import Button from '../Button';
 import './GameOverScreen.css';
+import { useEffect, useState } from 'react';
 
 const GameOverScreen = ({ setLives, setScore, setData, score }) => {
+  const [animation, setAnimation] = useState(false);
   //Handle restart the Game
   const restartGame = () => {
     setLives(3);
@@ -9,10 +11,18 @@ const GameOverScreen = ({ setLives, setScore, setData, score }) => {
     setData([{}]);
   };
 
+  useEffect(() => {
+    setAnimation(true);
+  }, []);
+
   return (
     <div className="gameover-container">
       <header className="gameover-container-header">
-        <div className="gameOverScreen">
+        <div
+          className={
+            animation ? 'gameOverScreen fade-in' : 'gameOverScreen hidden'
+          }
+        >
           <h1>GAME OVER</h1>
           <h5>Score: {score} </h5>
           <h5>High Score: --- </h5>
