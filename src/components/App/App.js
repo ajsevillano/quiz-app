72f2a7b4364fc2d0b83e76d98e3dc5535b66cc45
Sevi <correosevi@gmail.com>
2022-02-12 16:12:52 +0000

    Refactory screen handler

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,19 +9,45 @@ import MenuScreen from '../MenuScreen';
 import fetchQuestion from '../../libs/fetch.js';
 
 function App() {
-  const initialLives = 3;
+  const initialStates = {
+    initialLives: 3,
+    initialScore: 0,
+    initialScreens: [
+      { name: 'menu', isActive: true },
+      { name: 'game-over', isActive: false },
+      { name: 'game', isActive: false },
+    ],
+  };
+
   const highScore = Number(window.localStorage.getItem('highScore'));
   const [data, setData] = useState([{}]);
-  const [score, setScore] = useState(0);
+  const [score, setScore] = useState(initialStates.initialScore);
   const [answers, setAnswers] = useState([]);
-  const [lives, setLives] = useState(initialLives);
-  const [startScreen, setStartScreen] = useState(true);
+  const [lives, setLives] = useState(initialStates.initialLives);
+  const [gameScreens, setGameScreens] = useState(initialStates.initialScreens);
 
+  //Create or Update the highscore
   useEffect(() => {
     highScore.length === 0
       ? window.localStorage.setItem('highScore', 0)
       : score > highScore && window.localStorage.setItem('highScore', score);
   });
+
+  //Check if the game is over
+  useEffect(() => {
+    const setStates = () => {
+      const setGameOverScreen = gameScreens.map((obj) => {
+        return obj.name === 'game-over'
+          ? { ...obj, isActive: true }
+          : { ...obj, isActive: false };
+      });
+      setLives(0);
+      setGameScreens(setGameOverScreen);
+    };
+
+    //If lives are less than 0 load the game over screen
+    lives === -1 && setStates();
+  }, [gameScreens, lives]);
 
   //Update the score and the lives
   function updateScoreAndLives(answer) {
@@ -33,49 +59,69 @@ function App() {
 
   //When game is over it will reset all the states
   const resetGame = () => {
+    const setGameScreen = gameScreens.map((obj) => {
+      return obj.name === 'game'
+        ? { ...obj, isActive: true }
+        : { ...obj, isActive: false };
+    });
     setLives(3);
-    setScore(0);
     setData([{}]);
     setAnswers([]);
-    setStartScreen(false);
+    setGameScreens(setGameScreen);
     fetchQuestion(setData, setAnswers);
   };
 
-  return startScreen ? (
-    <MenuScreen>
-      <img className="logo-quiz" src="logo-quiz.png" alt="Logo" />
-      <h3 className="welcome-quiz">Welcome to Quiz Game!</h3>
-      <h5 className="choose-difficulty">Choose dificulty </h5>
-      <Button buttonText="Start" handleClick={resetGame} color="purple" />
-    </MenuScreen>
-  ) : lives < 0 ? (
-    //If lives go under 0, show the game over screen
-    <MenuScreen>
-      <img className="trophy-icon" src="./trophy.png" alt="trophy" />
-      <h5 className="high-score">High Score: {highScore} </h5>
-      <h1>GAME OVER</h1>
-      <p className="game-over-subtext">Well done! Don't give up!</p>
-      <h5 className="score-text">
-        Score: <span className="score-number"> {score}</span>
-      </h5>
-      <div className="menu-buttons-container">
-        <Button buttonText="Menu" handleClick={resetGame} color="purple" />
-        <Button buttonText="Play again" handleClick={resetGame} color="green" />
-      </div>
-    </MenuScreen>
-  ) : (
-    <Game
-      score={score}
-      setScore={setScore}
-      initialLives={initialLives}
-      lives={lives}
-      setLives={setLives}
-      data={data}
-      answers={answers}
-      updateScoreAndLives={updateScoreAndLives}
-      setData={setData}
-      setAnswers={setAnswers}
-    />
+  //Destructure the screens
+  const [mainMenu, gameover, game] = gameScreens;
+
+  return (
+    <>
+      {mainMenu.isActive && (
+        <MenuScreen>
+          <img className="logo-quiz" src="logo-quiz.png" alt="Logo" />
+          <h3 className="welcome-quiz">Welcome to Quiz Game!</h3>
+          <h5 className="choose-difficulty">Choose dificulty </h5>
+          <Button
+            buttonText="Start"
+            handleClick={() => resetGame()}
+            color="purple"
+          />
+        </MenuScreen>
+      )}
+      {game.isActive && (
+        <Game
+          score={score}
+          setScore={setScore}
+          initialLives={initialStates.initialLives}
+          lives={lives}
+          setLives={setLives}
+          data={data}
+          answers={answers}
+          updateScoreAndLives={updateScoreAndLives}
+          setData={setData}
+          setAnswers={setAnswers}
+        />
+      )}
+      {gameover.isActive && (
+        <MenuScreen>
+          <img className="trophy-icon" src="./trophy.png" alt="trophy" />
+          <h5 className="high-score">High Score: {highScore} </h5>
+          <h1>GAME OVER</h1>
+          <p className="game-over-subtext">Well done! Don't give up!</p>
+          <h5 className="score-text">
+            Score: <span className="score-number"> {score}</span>
+          </h5>
+          <div className="menu-buttons-container">
+            <Button buttonText="Menu" handleClick={resetGame} color="purple" />
+            <Button
+              buttonText="Play again"
+              handleClick={resetGame}
+              color="green"
+            />
+          </div>
+        </MenuScreen>
+      )}
+    </>
   );
 }
 
